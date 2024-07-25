4507866d8a0c83d6527ea54b307d618b9e9708f9
Angga Irham Stianto <anggairham08@gmail.com>
2024-07-25 11:06:34 +0700

    update

--- controllers/homecontroller/homecontroller.go
+++ controllers/homecontroller/homecontroller.go
@@ -5,11 +5,11 @@ import (
 	"net/http"
 )
 
-func Welcome(web http.ResponseWriter, request *http.Request){
+func Welcome(web http.ResponseWriter, request *http.Request) {
 	temp, err := template.ParseFiles("views/home/index.html")
-	if err!=nil {
+	if err != nil {
 		panic(err)
 	}
 
-	temp.Execute(web,nil)
+	temp.Execute(web, nil)
 }
